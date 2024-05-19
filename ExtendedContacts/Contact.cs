4024c8db418fd984b56db711f673c132324283fb
theKyrylo <zhulin.pers0n4l@gmail.com>
2024-05-19 15:56:09 +0200

    Created Account and Contact classes, added GUI for contact

---
+++ ExtendedContacts/Contact.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ExtendedContacts
+{
+    [Serializable]
+    public class Contact
+    {
+        public string ImagePath { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public DateTime DateOfBitrh { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+    }
+}
