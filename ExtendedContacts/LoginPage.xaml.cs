076895508a23273cbfbf3cdd29ee60c2e017b896
theKyrylo <zhulin.pers0n4l@gmail.com>
2024-05-19 15:10:45 +0200

    Created GUI for Login and CreateAccount pages

---
+++ ExtendedContacts/LoginPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExtendedContacts
+{
+    /// <summary>
+    /// Interaction logic for Page1.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CreateOneButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new CreateAccount(), DateTime.Now);
+        }
+    }
+}
